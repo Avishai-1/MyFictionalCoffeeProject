ea61b163ee643642bbc942ee0656d45008bba870
Avishai-1 <86708785+Avishai-1@users.noreply.github.com>
2021-08-14 07:00:41 +0100

    initialize file progress

---
+++ main.js
@@ -0,0 +1,8 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width
+     = '180px' ;
+}
+     function closeSlideMenu(){
+    document.getElementById('side-menu').style.width
+     = '0';
+}
